7ea4db14548e9a69dd92aa0bfcbfb734def746db
shgithubaccount <160748915+shgithubaccount@users.noreply.github.com>
2024-03-24 12:13:48 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//upper case
+var sentence = "i m doing this work";
+var uppercase = sentence.toUpperCase();
+console.log(uppercase);
+//lower case
+var words = "i m learning lowecase";
+var lowercase = words.toLowerCase();
+console.log(lowercase);
+//title case
+var ali = "i m learning titlecase";
+//step1
+var SHIFA = word.split("");
+//step2
+var TitleCase = "";
+//step3
+for (var i = 0; i < word.length; i++)
+    ;
+titlecase += word[1].charAt(0).toUpperCase() + words[1].slice(1).toLowerCase() + "";
+console.log(titlecase);
